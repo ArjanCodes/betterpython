4151e184748856cbb3ea32ff8ef1bcca448e6a68
Arjan Egges <arjan@quarterfall.com>
2021-05-03 17:14:19 +0200

    Simplified calling singleton super()

--- 10 - object creation/singleton.py
+++ 10 - object creation/singleton.py
@@ -2,7 +2,7 @@ class Singleton(type):
     _instances = {}
     def __call__(cls, *args, **kwargs):
         if cls not in cls._instances:
-            cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
+            cls._instances[cls] = super().__call__(*args, **kwargs)
         return cls._instances[cls]
         
 class Logger(metaclass=Singleton):
