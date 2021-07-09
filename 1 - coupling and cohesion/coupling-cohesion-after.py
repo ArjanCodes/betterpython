95313718b2566763a6cc8694a76a6b106ca9d1ff
motherboard <cantfind@gmail.com>
2021-07-10 00:12:46 +0300

    get rid of redundant class

--- 1 - coupling and cohesion/coupling-cohesion-after.py
+++ 1 - coupling and cohesion/coupling-cohesion-after.py
@@ -1,29 +1,33 @@
-import string
+from enum import Enum
+from dataclasses import dataclass
 import random
+import string
 
+ELECTRIC_TAX = 0.02
+REGULAR_TAX = 0.02
+
+
+@dataclass
 class VehicleInfo:
-    
-    def __init__(self, brand, electric, catalogue_price):
-        self.brand = brand
-        self.electric = electric
-        self.catalogue_price = catalogue_price
-
-    def compute_tax(self):
-        tax_percentage = 0.05
-        if self.electric:
-            tax_percentage = 0.02
-        return tax_percentage * self.catalogue_price
+    brand_name: str
+    electric: bool
+    catalogue_price: float
 
     def print(self):
-        print(f"Brand: {self.brand}")
+        print(f"Brand: {self.brand_name}")
         print(f"Payable tax: {self.compute_tax()}")
 
-class Vehicle:
+    def compute_tax(self):
+        if self.electric:
+            return ELECTRIC_TAX * self.catalogue_price
+        return REGULAR_TAX * self.catalogue_price
 
-    def __init__(self, id, license_plate, info):
-        self.id = id
-        self.license_plate = license_plate
-        self.info = info
+
+@dataclass
+class Vehicle:
+    id: str
+    license_plate: str
+    info: VehicleInfo
 
     def print(self):
         print(f"Id: {self.id}")
@@ -31,40 +35,29 @@ class Vehicle:
         self.info.print()
 
 
-class VehicleRegistry:
-
-    def __init__(self):
-        self.vehicle_info = { }
-        self.add_vehicle_info("Tesla Model 3", True, 60000)
-        self.add_vehicle_info("Volkswagen ID3", True, 35000)
-        self.add_vehicle_info("BMW 5", False, 45000)
-        self.add_vehicle_info("Tesla Model Y", True, 75000)
-
-    def add_vehicle_info(self, brand, electric, catalogue_price):
-        self.vehicle_info[brand] = VehicleInfo(brand, electric, catalogue_price)
-
-    def generate_vehicle_id(self, length):
-        return ''.join(random.choices(string.ascii_uppercase, k=length))
-
-    def generate_vehicle_license(self, id):
-        return f"{id[:2]}-{''.join(random.choices(string.digits, k=2))}-{''.join(random.choices(string.ascii_uppercase, k=2))}"
-
-    def create_vehicle(self, brand):
-        id = self.generate_vehicle_id(12)
-        license_plate = self.generate_vehicle_license(id)
-        return Vehicle(id, license_plate, self.vehicle_info[brand])
+class VehicleBrands(Enum):
+    BMW_5 = VehicleInfo('BMW 5', False, 45000)
+    TESLA_3 = VehicleInfo('Tesla Model 3', True, 60000)
+    VW_ID3 = VehicleInfo('Volkswagen ID3', True, 35000)
+    TESLA_Y = VehicleInfo('Tesla Model Y', True, 75000)
 
 
-class Application:
+def generate_vehicle_id(length: int):
+    return ''.join(random.choices(string.ascii_uppercase, k=length))
 
-    def register_vehicle(self, brand: string):
-        # create a registry instance
-        registry = VehicleRegistry()
 
-        vehicle = registry.create_vehicle(brand)
+def generate_vehicle_license(id: str):
+    return f"{id[:2]}-{''.join(random.choices(string.digits, k=2))}-{''.join(random.choices(string.ascii_uppercase, k=2))}"
 
-        # print out the vehicle information
-        vehicle.print()
 
-app = Application()
-app.register_vehicle("Volkswagen ID3")
+def create_vehicle(brand: VehicleBrands):
+    id = generate_vehicle_id(12)
+    license_plate = generate_vehicle_license(id)
+    return Vehicle(id, license_plate, brand.value)
+
+
+def register_vehicle(brand: VehicleBrands):
+    create_vehicle(brand).print()
+
+
+register_vehicle(VehicleBrands.VW_ID3)
