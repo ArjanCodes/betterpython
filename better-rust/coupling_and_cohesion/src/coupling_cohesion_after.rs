1c98122da9dfc80d75c23f7355b8bd12b61441a5
Diego Viniegra <diego@bearing.ai>
2024-04-29 15:11:28 -0500

    Add coupling cohesion after

--- better-rust/coupling_and_cohesion/src/coupling_cohesion_after.rs
+++ better-rust/coupling_and_cohesion/src/coupling_cohesion_after.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use rand::{self, Rng};
 
 fn generate_random_string(length: usize, digits: bool) -> String {
@@ -10,9 +11,71 @@ fn generate_random_string(length: usize, digits: bool) -> String {
         .collect()
 }
 
-pub struct VehicleRegistry;
+#[derive(Clone)]
+pub struct VehicleInfo {
+    pub brand: String,
+    pub electric: bool,
+    pub catalogue_price: u32,
+}
+
+impl VehicleInfo {
+    pub fn new(brand: &str, electric: bool, catalogue_price: u32) -> Self {
+        Self { brand: brand.to_string(), electric, catalogue_price }
+    }
+
+    pub fn compute_tax(&self) -> f32 {
+        let mut tax_percentage = 0.05;
+        if self.electric {
+            tax_percentage = 0.02;
+        }
+        
+        tax_percentage * self.catalogue_price as f32
+    }
+
+    pub fn print(&self) {
+        println!("Brand: {}", self.brand);
+        println!("Payable tax: {:#?}", self.compute_tax());
+    }
+}
+
+pub struct Vehicle {
+    pub id: String,
+    pub license_plate: String,
+    pub info: VehicleInfo,
+}
+
+impl Vehicle {
+    pub fn new(id: &str, license_plate: String, info: VehicleInfo) -> Self {
+        Self { id: id.to_string(), license_plate, info }
+    }
+
+    pub fn print(&self) {
+        println!("Registration complete. Vehicle information:");
+        println!("Id: {}", self.id);
+        println!("License plate: {}", self.license_plate);
+        self.info.print();
+    }
+}
+pub struct VehicleRegistry {
+    pub vehicle_info: HashMap<String, VehicleInfo>,
+}
 
 impl VehicleRegistry {
+    pub fn new() -> Self {
+        let mut vehicle_registry = Self { vehicle_info: HashMap::new() };
+
+        vehicle_registry.add_vehicle_info("Tesla Model 3", true, 60000);
+        vehicle_registry.add_vehicle_info("Volkswagen ID3", true, 35000);
+        vehicle_registry.add_vehicle_info("BMW 5", false, 45000);
+        vehicle_registry.add_vehicle_info("Tesla Model Y", true, 75000);
+
+        vehicle_registry
+    }
+
+    fn add_vehicle_info(&mut self, brand: &str, electric: bool, catalogue_price: u32) {
+        self.vehicle_info.insert(brand.to_string(), VehicleInfo::new(brand, electric, catalogue_price));
+    }
+
     pub fn generate_vehicle_id(&self, length: usize) -> String {
         generate_random_string(length, false)
     }
@@ -24,48 +87,25 @@ impl VehicleRegistry {
 
         format!("{front}-{middle}-{last}")
     }
+
+    pub fn create_vehicle(&self, brand: &str) -> Vehicle {
+        let id = self.generate_vehicle_id(12);
+        let license_plate = self.generate_vehicle_license(&id);
+
+        Vehicle::new(&id, license_plate, self.vehicle_info.get(brand).unwrap().clone())
+    }
 }
 
 pub struct Application;
 
 impl Application {
     pub fn register_vehicle(&self, brand: &str) {
-        // create a registry instance
-        let registry = VehicleRegistry;
+    // create a registry instance
+    let registry = VehicleRegistry::new();
 
-        // generate a vehicle id of length 12
-        let vehicle_id = registry.generate_vehicle_id(12);
+    let vehicle = registry.create_vehicle(brand);
 
-        // now generate a license plate for the vehicle
-        // using the first two characters of the vehicle id
-        let license_plate = registry.generate_vehicle_license(&vehicle_id);
-
-        // compute the catalogue price
-        let mut catalogue_price = 0;
-
-        if brand == "Tesla Model 3" {
-            catalogue_price = 60000;
-        } else if brand == "Volkswagen ID3" {
-            catalogue_price = 35000;
-        } else if brand == "BMW 5" {
-            catalogue_price = 45000;
-        }
-
-        // compute the tax percentage (default 5% of the catalogue price, except for electric cars where it is 2%)
-        let mut tax_percentage = 0.05;
-
-        if brand == "Tesla Model 3" || brand == "Volkswagen ID3" {
-            tax_percentage = 0.02;
-        }
-
-        // compute the payable tax
-        let payable_tax = tax_percentage * catalogue_price as f32;
-
-        // print out the vehicle registration information
-        println!("Registration complete. Vehicle information:");
-        println!("Brand: {brand}");
-        println!("Id: {vehicle_id}");
-        println!("License plate: {license_plate}");
-        println!("Payable tax: {payable_tax:?}");
+    // print out the vehicle information
+    vehicle.print();
     }
 }
